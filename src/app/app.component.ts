06d7b48cd8c603df26935289f1be9a9312791178
cwei1124 <eric2003921124@gmail.com>
2025-04-19 21:03:47 +0800

    feat(search): 產品查詢框樣式實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
