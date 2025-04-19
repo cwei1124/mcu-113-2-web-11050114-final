d5182f0f7bf72d13784f0f3261de3b733655a898
cwei1124 <eric2003921124@gmail.com>
2025-04-19 22:07:44 +0800

    feat(product-card): 利用文字插值顯示產品名稱、作者與公司資訊

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -4,8 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-product-card',
   imports: [],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
-
+  productName = 'A 產品';
+  author = '作者A、作者B、作者C';
+  company = '博碩文化';
 }
