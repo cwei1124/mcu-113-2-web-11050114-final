dcd741153e241fbaf2ada4536a2ec7f14f0794c8
cwei1124 <eric2003921124@gmail.com>
2025-04-23 22:18:08 +0800

    feat(product-page): 新增產品清單頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
-import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, SearchComponent, ProductCardListComponent],
+  imports: [HeaderComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  products: Product[] = [
-    new Product({
-      id: 1,
-      name: 'A 產品',
-      authors: ['作者A', '作者B', '作者C'],
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 2,
-      name: 'B 產品',
-      authors: ['作者A', '作者B', '作者C'],
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 3,
-      name: 'C 產品',
-      authors: ['作者A', '作者B', '作者C'],
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 4,
-      name: 'D 產品',
-      authors: ['作者A', '作者B', '作者C'],
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 5,
-      name: 'E 產品',
-      authors: ['作者A', '作者B', '作者C'],
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-  ];
-}
+export class AppComponent {}
---
+++ src/app/product-page/product-page.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-product-page',
+  imports: [ProductCardListComponent],
+  templateUrl: './product-page.component.html',
+  styleUrl: './product-page.component.scss',
+})
+export class ProductPageComponent {
+  products: Product[] = [
+    new Product({
+      id: 1,
+      name: 'A 產品',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+    new Product({
+      id: 2,
+      name: 'B 產品',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+    new Product({
+      id: 3,
+      name: 'C 產品',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+    new Product({
+      id: 4,
+      name: 'D 產品',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+    new Product({
+      id: 5,
+      name: 'E 產品',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+  ];
+}
