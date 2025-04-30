8b38c7bb3750f137939d017732599374b0c94856
cwei1124 <eric2003921124@gmail.com>
2025-04-30 19:03:29 +0800

    feat(service): 從服務取得產品資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,8 +1,9 @@
 import { SearchComponent } from './../search/search.component';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Product } from '../models/product';
 import { Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -10,51 +11,17 @@ import { Router } from '@angular/router';
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit {
   private router = inject(Router);
 
-  products: Product[] = [
-    new Product({
-      id: 1,
-      name: 'A 產品',
-      authors: ['作者A', '作者B', '作者C'],
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 2,
-      name: 'B 產品',
-      authors: ['作者A', '作者B', '作者C'],
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 3,
-      name: 'C 產品',
-      authors: ['作者A', '作者B', '作者C'],
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 4,
-      name: 'D 產品',
-      authors: ['作者A', '作者B', '作者C'],
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 5,
-      name: 'E 產品',
-      authors: ['作者A', '作者B', '作者C'],
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-  ];
+  private productService!: ProductService;
+
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.productService = new ProductService();
+    this.products = this.productService.getList();
+  }
 
   onView(product: Product): void {
     this.router.navigate(['product', product.id]);
