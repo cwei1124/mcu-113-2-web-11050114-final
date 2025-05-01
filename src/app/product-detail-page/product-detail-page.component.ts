c109ab485e1efb765a49a4c8af9ca2222d904345
cwei1124 <eric2003921124@gmail.com>
2025-05-01 13:05:14 +0800

    feat(product-detail): 實作依產品編號取得明細資料

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,7 +1,8 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, input, numberAttribute, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../models/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-detail-page',
@@ -9,17 +10,18 @@ import { Product } from '../models/product';
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.scss',
 })
-export class ProductDetailPageComponent {
-  product = new Product({
-    id: 9,
-    name: 'I 產品',
-    authors: ['作者A', '作者B', '作者C'],
-    company: '博碩文化',
-    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-    price: 1580,
-  });
+export class ProductDetailPageComponent implements OnInit {
+  id = input.required<number, string | number>({ transform: numberAttribute });
+
+  product!: Product;
 
   readonly router = inject(Router);
+
+  private productService = inject(ProductService);
+
+  ngOnInit(): void {
+    this.product = this.productService.getById(this.id());
+  }
 
   onBack(): void {
     this.router.navigate(['products']);
