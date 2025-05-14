fdb10f180f116cc450658700633c34dfa55916da
cwei1124 <eric2003921124@gmail.com>
2025-05-14 21:09:49 +0800

    feat(product): 實作 Observable 的分頁機制

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { delay, filter, mergeMap, Observable, of } from 'rxjs';
+import { delay, filter, map, mergeMap, Observable, of, toArray } from 'rxjs';
 import { Product } from '../models/product';
 
 @Injectable({
@@ -97,9 +97,16 @@ export class ProductService {
   }
 
   getList(name: string | undefined, index: number, size: number): Observable<{ data: Product[]; count: number }> {
-    const startIndex = (index - 1) * size;
-    const endIndex = startIndex + size;
-    const data = name ? this._data.filter((item) => item.name === name) : [...this._data];
-    return of({ data: data.slice(startIndex, endIndex), count: this._data.length }).pipe(delay(1000));
+    return of(this._data).pipe(
+      mergeMap((data) => data),
+      filter((item) => (name ? item.name === name : true)),
+      toArray(),
+      map((data) => {
+        const startIndex = (index - 1) * size;
+        const endIndex = startIndex + size;
+        return { data: data.slice(startIndex, endIndex), count: data.length };
+      }),
+      delay(500)
+    );
   }
 }
