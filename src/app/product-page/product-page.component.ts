a6682e4f6b747c81a0c8531df81811345ec263e7
cwei1124 <eric2003921124@gmail.com>
2025-05-14 21:26:43 +0800

    refactor(product): 變更使用 to signal 轉換 observable

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,11 +1,12 @@
 import { SearchComponent } from './../search/search.component';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { BehaviorSubject, Subject, combineLatest, startWith, switchMap, tap } from 'rxjs';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Product } from '../models/product';
 import { Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
 import { PaginationComponent } from '../pagination/pagination.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-page',
@@ -13,7 +14,7 @@ import { PaginationComponent } from '../pagination/pagination.component';
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
-export class ProductPageComponent implements OnInit {
+export class ProductPageComponent {
   private router = inject(Router);
 
   private productService = inject(ProductService);
@@ -30,24 +31,25 @@ export class ProductPageComponent implements OnInit {
 
   pageSize = 5;
 
-  totalCount = 0;
+  private readonly data$ = combineLatest([
+    this.pageIndex$.pipe(tap((value) => console.log('page index', value))),
+    this.refresh$.pipe(
+      startWith(undefined),
+      tap(() => console.log('refresh'))
+    ),
+  ]).pipe(switchMap(() => this.productService.getList(undefined, this.pageIndex, this.pageSize)));
 
-  products: Product[] = [];
+  private readonly data = toSignal(this.data$, { initialValue: { data: [], count: 0 } });
 
-  ngOnInit(): void {
-    combineLatest([
-      this.pageIndex$.pipe(tap((value) => console.log('page index', value))),
-      this.refresh$.pipe(
-        startWith(undefined),
-        tap(() => console.log('refresh'))
-      ),
-    ])
-      .pipe(switchMap(() => this.productService.getList(undefined, this.pageIndex, this.pageSize)))
-      .subscribe(({ data, count }) => {
-        this.products = data;
-        this.totalCount = count;
-      });
-  }
+  readonly totalCount = computed(() => {
+    const { count } = this.data();
+    return count;
+  });
+
+  readonly products = computed(() => {
+    const { data } = this.data();
+    return data;
+  });
 
   onView(product: Product): void {
     this.router.navigate(['product', product.id]);
