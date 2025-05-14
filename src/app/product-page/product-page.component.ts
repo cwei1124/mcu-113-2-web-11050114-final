6485e47ab0a51eedd0a36f45a6acda1aee54564f
cwei1124 <eric2003921124@gmail.com>
2025-05-14 21:23:02 +0800

    feat(product): 實作資料分頁後重新取得資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
 import { SearchComponent } from './../search/search.component';
 import { Component, OnInit, inject } from '@angular/core';
+import { BehaviorSubject, Subject, combineLatest, startWith, switchMap, tap } from 'rxjs';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Product } from '../models/product';
 import { Router } from '@angular/router';
@@ -17,7 +18,15 @@ export class ProductPageComponent implements OnInit {
 
   private productService = inject(ProductService);
 
-  pageIndex = 1;
+  private readonly pageIndex$ = new BehaviorSubject(1);
+  get pageIndex() {
+    return this.pageIndex$.value;
+  }
+  set pageIndex(value: number) {
+    this.pageIndex$.next(value);
+  }
+
+  private readonly refresh$ = new Subject<void>();
 
   pageSize = 5;
 
@@ -26,22 +35,21 @@ export class ProductPageComponent implements OnInit {
   products: Product[] = [];
 
   ngOnInit(): void {
-    this.getProducts();
+    combineLatest([
+      this.pageIndex$.pipe(tap((value) => console.log('page index', value))),
+      this.refresh$.pipe(
+        startWith(undefined),
+        tap(() => console.log('refresh'))
+      ),
+    ])
+      .pipe(switchMap(() => this.productService.getList(undefined, this.pageIndex, this.pageSize)))
+      .subscribe(({ data, count }) => {
+        this.products = data;
+        this.totalCount = count;
+      });
   }
 
   onView(product: Product): void {
     this.router.navigate(['product', product.id]);
-  }
-
-  onPageIndexChange(pageIndex: number): void {
-    this.pageIndex = pageIndex;
-    this.getProducts();
-  }
-
-  private getProducts(): void {
-    this.productService.getList(undefined, this.pageIndex, this.pageSize).subscribe(({ data, count }) => {
-      this.products = data;
-      this.totalCount = count;
-    });
   }
 }
