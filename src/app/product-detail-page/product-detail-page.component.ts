ee03d6f95dfc3be49767c8539d494ff58b23d741
cwei1124 <eric2003921124@gmail.com>
2025-05-21 21:52:52 +0800

    feat(product-detail): 實作折扣明細樣式

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,12 +1,11 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component, inject, input, numberAttribute, OnInit } from '@angular/core';
+import { CurrencyPipe, NgIf } from '@angular/common';
+import { Component, inject, input, model } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../models/product';
-import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-detail-page',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, NgIf],
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.scss',
 })
@@ -14,6 +13,8 @@ export class ProductDetailPageComponent {
   readonly product = input.required<Product>();
 
   readonly router = inject(Router);
+
+  readonly discount = model.required<boolean>();
 
   onBack(): void {
     this.router.navigate(['products']);
