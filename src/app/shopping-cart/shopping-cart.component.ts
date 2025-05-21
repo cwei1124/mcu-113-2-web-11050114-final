5a346379c8f528ad565c26bc4bafef318b93f8be
cwei1124 <eric2003921124@gmail.com>
2025-05-21 22:58:07 +0800

    feat(shopping-cart): 購物車表單姓名、地址、電話欄位設定

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,11 +1,17 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-cart',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './shopping-cart.component.html',
-  styleUrl: './shopping-cart.component.scss'
+  styleUrl: './shopping-cart.component.scss',
 })
 export class ShoppingCartComponent {
-
+  form = new FormGroup({
+    name: new FormControl<string | null>(null),
+    address: new FormControl<string | null>(null),
+    phone: new FormControl<string | null>(null),
+  });
 }
