1cdb3010b0ef492b5d5a48a73ae8b5ed7f1a1c64
cwei1124 <eric2003921124@gmail.com>
2025-05-27 15:58:03 +0800

    style(search): 刪除查詢 component 及不重要程式碼

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { Product } from './models/product';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
